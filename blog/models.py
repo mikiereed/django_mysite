dce1d2d769f2b2e57d3ed658b63d6405cfc54fed
mikie Reed <mikie@prenotions.com>
2020-08-10 15:23:54 -0700

    start HTML

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+            .filter(status='published')
 
 
 class Post(models.Model):
@@ -24,3 +32,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
